a5cf7cf037af0cc62ac8c73a63b292c56ddc2886
valentina Esteban <valentina.esteban@ug.uchile.cl>
2022-06-05 01:57:30 -0400

    nose

--- T3/cgi-bin/portada.py
+++ T3/cgi-bin/portada.py
@@ -48,11 +48,13 @@ enHead = """
 
         
         <link rel="stylesheet" href="../leaflet/leaflet.css" />
-        <script src="../leaflet/leaflet.js"></script>
-        <script src= ../mis-js/mapa.js></script>
 
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
+
+
+        <!-- no pongo esto porq importe los documentos estan mas abajo 
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin=""/>
         <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
+         -->
         """
 
 with open('templates/template1.html','r', encoding='utf-8') as template:
@@ -80,5 +82,7 @@ with open('templates/template1.html','r', encoding='utf-8') as template:
     </div>
     <div class= 'rectangulo_contenido' id = 'map'>
     </div>
+    <script src="../leaflet/leaflet.js"></script>
+    <script src= ../mis-js/mapa.js></script>
     """))
     
